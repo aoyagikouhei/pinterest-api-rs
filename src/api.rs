51aed514ba01d243002ae8dfce191bd478efc67e
Kouhei Aoyagi <aoyagi_kouhei@uniquevision.co.jp>
2024-10-03 08:32:51 +0900

    add delete board section

--- src/api.rs
+++ src/api.rs
@@ -5,6 +5,7 @@ use std::collections::HashMap;
 use crate::error::{Error, ErrorResponse};
 
 pub mod delete_boards_board_id;
+pub mod delete_boards_board_id_sections_section_id;
 pub mod delete_pins_pin_id;
 pub mod get_boards;
 pub mod get_boards_board_id;
--- src/api/delete_boards_board_id_sections_section_id.rs
+++ src/api/delete_boards_board_id_sections_section_id.rs
@@ -1,17 +1,11 @@
 use reqwest::RequestBuilder;
-use serde::{Deserialize, Serialize};
 
 use crate::{
-    api::{execute_api, ApiResponse},
     error::Error,
     options::{apply_options, make_url, ApiOptions},
-    response::board_section::BoardSection,
 };
 
-#[derive(Serialize, Deserialize, Debug, Default, Clone)]
-pub struct Body {
-    pub name: String,
-}
+use super::{execute_empty_api, ApiEmptyResponse};
 
 #[derive(Debug, Clone, Default)]
 pub struct Api {
@@ -19,14 +13,12 @@ pub struct Api {
     board_id: String,
     section_id: String,
     ad_account_id: Option<String>,
-    body: Body,
 }
 
 impl Api {
-    pub fn new(options: Option<ApiOptions>, body: Body, board_id: &str, section_id: &str) -> Self {
+    pub fn new(options: Option<ApiOptions>, board_id: &str, section_id: &str) -> Self {
         Self {
             options,
-            body,
             board_id: board_id.to_string(),
             section_id: section_id.to_string(),
             ..Default::default()
@@ -44,18 +36,17 @@ impl Api {
             query_parameters.push(("ad_account_id", ad_account_id));
         }
         let client = reqwest::Client::new()
-            .patch(make_url(
+            .delete(make_url(
                 &format!("/boards/{}/sections/{}", self.board_id, self.section_id),
                 &self.options,
             ))
             .query(&query_parameters)
-            .json(&self.body)
             .bearer_auth(bearer_code);
         apply_options(client, &self.options)
     }
 
-    pub async fn execute(self, bearer_code: &str) -> Result<ApiResponse<BoardSection>, Error> {
-        execute_api(self.build(bearer_code)).await
+    pub async fn execute(self, bearer_code: &str) -> Result<ApiEmptyResponse, Error> {
+        execute_empty_api(self.build(bearer_code)).await
     }
 }
 
@@ -63,17 +54,14 @@ impl Api {
 mod tests {
     use super::*;
 
-    // BEARER_CODE=xxx BOARD_ID=xxx SECTION_ID=xxx cargo test test_patch_boards_board_id_sections_section_id -- --nocapture --test-threads=1
+    // BEARER_CODE=xxx BOARD_ID=xxx SECTION_ID=xxx cargo test test_delete_boards_board_id_sections_section_id -- --nocapture --test-threads=1
 
     #[tokio::test]
-    async fn test_patch_boards_board_id_sections_section_id() {
+    async fn test_delete_boards_board_id_sections_section_id() {
         let bearer_code = std::env::var("BEARER_CODE").unwrap_or_default();
         let board_id = std::env::var("BOARD_ID").unwrap_or_default();
         let section_id = std::env::var("SECTION_ID").unwrap_or_default();
-        let body = Body {
-            name: "test".to_owned(),
-        };
-        let response = Api::new(None, body, &board_id, &section_id)
+        let response = Api::new(None, &board_id, &section_id)
             .execute(bearer_code.as_str())
             .await
             .unwrap();
